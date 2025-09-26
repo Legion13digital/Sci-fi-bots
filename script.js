1991bb83ef0ae363eb9f64eff1f8c78230e1a324
LegionFrontend <cagej7517@gmail.com>
2025-09-26 20:33:32 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+  // Плавная прокрутка при нажатии на ссылки
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        if (targetId === '#') return;
+        
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 80,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
